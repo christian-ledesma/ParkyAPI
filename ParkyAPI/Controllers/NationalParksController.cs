1167c59bd6f9ba66f2485363326c102b041f2740
Christian Ledesma <christian.ledesma@neoris.com>
2022-05-24 13:01:48 +0200

    NationalParksController created

---
+++ ParkyAPI/Controllers/NationalParksController.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using ParkyAPI.Repository.Interfaces;
+
+namespace ParkyAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NationalParksController : ControllerBase
+    {
+        private readonly INationalParkRepository _repository;
+        private readonly IMapper _mapper;
+
+        public NationalParksController(INationalParkRepository repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+    }
+}
