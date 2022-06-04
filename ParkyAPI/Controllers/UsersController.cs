f271c1f59cccfd8f4cb7d153cc9a123f8855cc58
Christian <christian_ledesma_a@hotmail.com>
2022-06-04 03:06:46 +0200

    Add UserCreateDto

--- ParkyAPI/Controllers/UsersController.cs
+++ ParkyAPI/Controllers/UsersController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using ParkyAPI.Models;
+using ParkyAPI.Models.DTOs.User;
 using ParkyAPI.Repository.Interfaces;
 using System.Threading.Tasks;
 
@@ -20,9 +21,9 @@ namespace ParkyAPI.Controllers
         [AllowAnonymous]
         [HttpPost]
         [Route("authenticate")]
-        public async Task<IActionResult> Authenticate([FromBody] User user)
+        public async Task<IActionResult> Authenticate([FromBody] UserCreateDto user)
         {
-            var response = await _userRepository.Authenticate(user.Name, user.Password);
+            var response = await _userRepository.Authenticate(user.Username, user.Password);
             if (response == null)
                 return BadRequest(new { response = "Username or Password is invalid" });
             return Ok(response);
@@ -31,14 +32,14 @@ namespace ParkyAPI.Controllers
         [AllowAnonymous]
         [HttpPost]
         [Route("register")]
-        public async Task<IActionResult> Register([FromBody] User user)
+        public async Task<IActionResult> Register([FromBody] UserCreateDto user)
         {
-            var isUserUnique = await _userRepository.UserIsUnique(user.Name);
+            var isUserUnique = await _userRepository.UserIsUnique(user.Username);
             if (!isUserUnique)
             {
                 return BadRequest(new { response = "User already exists" });
             }
-            var newUser = await _userRepository.Register(user.Name, user.Password);
+            var newUser = await _userRepository.Register(user.Username, user.Password);
             if (newUser == null)
             {
                 return BadRequest(new { response = "User not created due to a problem" });
---
+++ ParkyAPI/Models/DTOs/User/UserCreateDto.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ParkyAPI.Models.DTOs.User
+{
+    public class UserCreateDto
+    {
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+}
